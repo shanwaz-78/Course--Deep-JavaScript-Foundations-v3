0091f058592291b30756af07bcf06b67ee67e7fe
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-01-10 14:28:22 +0530

    learnt new deep methods

---
+++ js/introduction/introduction.js
@@ -0,0 +1,14 @@
+let num = 40;
+
+num++; // it's called postfixed position increament. 40
+console.log(num); // 41
+
+++num; // 42
+console.log(num); // 42
+
+let num2 = '5';
+num2 = num2 + 1; //'51'
+
+
+num2++ // 51
+console.log(num2) // 52
