ee30e0ff9c196244d4c991611b2365c1962f0e76
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-01-23 12:14:54 +0530

    learnt new thing

---
+++ Js/Equality/double equal corner case.js
@@ -0,0 +1,9 @@
+// if ([] == false) {, when we compare a primitive to non primitive we have to turn a non primitive.
+// into primitive. and what does he becomes he becomes empty string.
+
+// if ('' == false) {, now we have both primitive values but nor the same data types.
+// and algorithm prefers they both becomes number.
+
+// if (0 == false) {, now we have one number and also a boolean and it has to convert it into a number.
+
+// if (0 === 0) {, now we used strict equality coz now we have two numbers and even same data types.
