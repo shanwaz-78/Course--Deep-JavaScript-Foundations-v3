6c4411b4c6c13909d1b6d4d772e560ead02b5ce7
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-05-25 20:04:21 +0530

    Complted Corse

--- Js/types/Kinds of Emptiness.js
+++ Js/types/Kinds of Emptiness.js
@@ -1,5 +1,5 @@
     // Undefined. // Undefined is absolutely equal to itself.
     // Undeclared.  
     
-//  Undefined Means There's definitely a variable and at the moment it has no value.
+//  Undefined Means There's definitely a variable but at the moment we didn't initialize it with any value but there is one values implicitly exists on that type which is undefined..
 //  Undeclared Means It's never been declared in any scope. That we access to.
